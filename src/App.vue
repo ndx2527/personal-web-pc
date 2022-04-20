b1256512f22fc7c972ac0c552eeaf2cabd751809
ndx2527 <1378362527@qq.com>
2022-04-20 17:08:23 +0800

    feat：项目初始化

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-size: 14px;
+  background-color: rgb(250, 250, 250);
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
